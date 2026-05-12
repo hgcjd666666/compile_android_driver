37ca5b858e2c9dca55fea320f318711c36e44530
hgcjd666666 <1220118542@qq.com>
2026-05-12 09:34:22 +0800

    fix filter_maps: ARM64 register mapping, drop self dentry check, null checks

--- code/filter_maps.c
+++ code/filter_maps.c
@@ -5,11 +5,24 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/dcache.h>
 
 MODULE_LICENSE("GPL");
 
-static struct kprobe kp;
-static int orig_seq_read_return;
+#if defined(__aarch64__)
+#define REG_PARM0 regs[0]   /* file */
+#define REG_PARM1 regs[1]   /* buf */
+#define REG_PARM2 regs[2]   /* size */
+#define REG_PARM3 regs[3]   /* ppos */
+#define REG_RET   regs[0]   /* return value (copied) */
+#else
+#define REG_PARM0 di
+#define REG_PARM1 si
+#define REG_PARM2 dx
+#define REG_PARM3 cx
+#define REG_RET   ax
+#endif
 
 static int seq_read_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
@@ -18,33 +31,27 @@ static int seq_read_ret_handler(struct kretprobe_instance *ri, struct pt_regs *r
     loff_t *ppos;
     size_t copied;
     char *kbuf;
-    size_t size;
     size_t new_size = 0;
     char *new_buf;
     char *line_start, *line_end;
-    int i;
 
-    file = (struct file *)regs->di;
-    ppos = (loff_t *)regs->cx;
-    copied = regs->ax;
+    file = (struct file *)regs->REG_PARM0;
+    ppos = (loff_t *)regs->REG_PARM3;
+    copied = regs->REG_RET;
 
-    if (copied <= 0 || !file)
+    if (copied <= 0 || !file || !file->f_path.dentry)
         return 0;
 
     if (strcmp(file->f_path.dentry->d_name.name, "maps") != 0)
         return 0;
-    if (!file->f_path.dentry->d_parent ||
-        strcmp(file->f_path.dentry->d_parent->d_name.name, "self") != 0)
-        return 0;
 
     m = (struct seq_file *)file->private_data;
-    if (!m || !m->buf)
+    if (!m || !m->buf || m->count == 0)
         return 0;
 
     kbuf = m->buf;
-    size = m->size;
 
-    new_buf = kmalloc(size + 1, GFP_KERNEL);
+    new_buf = kmalloc(m->size + 1, GFP_KERNEL);
     if (!new_buf)
         return 0;
 
@@ -67,8 +74,9 @@ static int seq_read_ret_handler(struct kretprobe_instance *ri, struct pt_regs *r
         memcpy(kbuf, new_buf, new_size);
         m->count = new_size;
         m->index = 0;
-        *ppos = 0;
-        regs->ax = new_size;
+        if (ppos)
+            *ppos = 0;
+        regs->REG_RET = new_size;
     }
 
     kfree(new_buf);
@@ -87,17 +95,17 @@ static int __init filter_init(void)
 
     ret = register_kretprobe(&rp);
     if (ret < 0) {
-        printk(KERN_ERR "filter_maps: failed to register kretprobe on seq_read\n");
+        printk(KERN_ERR "filter_maps: failed to register kretprobe on seq_read, ret=%d\n", ret);
         return ret;
     }
-    printk(KERN_INFO "filter_maps: kretprobe registered, filtering 'KSU' lines in /proc/self/maps\n");
+    printk(KERN_INFO "filter_maps: loaded, filtering 'KSU' lines in /proc/self/maps\n");
     return 0;
 }
 
 static void __exit filter_exit(void)
 {
     unregister_kretprobe(&rp);
-    printk(KERN_INFO "filter_maps: module unloaded\n");
+    printk(KERN_INFO "filter_maps: unloaded\n");
 }
 
 module_init(filter_init);
